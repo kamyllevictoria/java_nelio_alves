d6a7d3228dbbed2c783e2d808a09a50a74ababbc
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-03-20 22:24:02 -0300

    hello world in javagit add .

---
+++ primeiro/src/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello, world!");
+        System.out.println("Nice to meet you!");
+        int y = 31;
+        double k =  120.239023;
+        System.out.printf("%.3f%n", k);
+        Locale.setDefault(Locale.US);
+    }
+
+}

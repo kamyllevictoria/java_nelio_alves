36becf1ee7a8f944fd07a7a407e252ad0b402cb7
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-03-23 18:42:02 -0300

    data input at java part II

--- primeiro/src/app/ScannerClass.java
+++ primeiro/src/app/ScannerClass.java
@@ -7,7 +7,7 @@ public class ScannerClass {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        
+
         char y;
 
         System.out.println("Please, type somethings!! :)");

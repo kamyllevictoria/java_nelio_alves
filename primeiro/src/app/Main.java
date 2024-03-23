e2c60ac4b7e09fd96d7f74bbe1d6c7366e27cc36
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-03-23 18:24:17 -0300

    data input at java part I

--- primeiro/src/app/Main.java
+++ primeiro/src/app/Main.java
@@ -4,6 +4,16 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
+
+        int b, a;
+        double result;
+
+        a = 5;
+        b = 2;
+
+        result = (double )a / b;
+        System.out.println(result);
+
         System.out.print("Hello, world!");
         System.out.println("Nice to meet you!");
         int y = 31;
@@ -11,5 +21,4 @@ public class Main {
         System.out.printf("%.3f%n", k);
         Locale.setDefault(Locale.US);
     }
-
 }

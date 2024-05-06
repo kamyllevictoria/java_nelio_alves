bebe2bef07ca2c68fc855cab94fe2814f06b09bf
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-05-06 10:03:38 -0300

    arrays exercises part II

--- java_nelio_alves-master/primeiro/src/arrays/exercise06.java
+++ java_nelio_alves-master/primeiro/src/arrays/exercise06.java
@@ -1,4 +1,39 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class exercise06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n;
+
+        System.out.println("How many positions will vectores A and B have?");
+        n = sc.nextInt();
+        double[]A = new double[n];
+        double[]B = new double[n];
+
+        System.out.println("Enter the A vector values: ");
+        for(int i = 0; i < A.length; i++){
+            A[i] = sc.nextDouble();
+        }
+
+        System.out.println("Enter the B vector values: ");
+        for(int i = 0; i < B.length; i++){
+            B[i] = sc.nextDouble();
+        }
+
+        double[]C = new double[n];
+        for(int i = 0; i< A.length; i++){
+            for(int j = 0; j < B.length; j++){
+                C[i] = A[i] + B[i];
+            }
+        }
+        System.out.println(Arrays.toString(C));
+
+
+
+
+        sc.close();
+    }
 }

b6402375486ff5871be47eef93dbe5ebc1e61fdc
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-05-08 16:06:28 -0300

    git commit -m "array exercise part III"

--- java_nelio_alves-master/primeiro/src/arrays/exercise07.java
+++ java_nelio_alves-master/primeiro/src/arrays/exercise07.java
@@ -1,4 +1,30 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class exercise07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n;
+        double media = 0, sum = 0;
+        System.out.println("How many elements will there be in the vector?");
+        n = sc.nextInt();
+
+        double[] array = new double[n];
+        for(int i = 0; i < array.length; i++){
+            System.out.println("Enter a number: ");
+            array[i] = sc.nextDouble();
+            sum += array[i];
+            media = sum/ array.length;
+        }
+
+        System.out.println("Array average: " + media);
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < media){
+                System.out.println("Elements below average: " + array[i]);
+            }
+        }
+
+        sc.close();
+    }
 }

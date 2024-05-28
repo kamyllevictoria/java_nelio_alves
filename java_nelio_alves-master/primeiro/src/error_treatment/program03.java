f032c74fd5b77f789dba4d7ff25ffb27ee5c47b3
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-05-28 20:05:30 -0300

    practice exercise about try-catch studies part III

--- java_nelio_alves-master/primeiro/src/error_treatment/program03.java
+++ java_nelio_alves-master/primeiro/src/error_treatment/program03.java
@@ -1,7 +1,51 @@
 package error_treatment;
 import error_treatment.model_entities.Reservation;
-public class program03 {
-    public static void main(String[] args) {
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class program03 {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner (System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Room number: ");
+        int number = sc.nextInt();
+
+        System.out.println("Check-in data (dd/mm/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+
+        System.out.println("Check-out data (dd/mm/yyyy): ");
+        Date checkOut = sdf.parse(sc.next());
+
+
+        if(!checkOut.after(checkIn)){
+            System.out.println("Error in reservation. Check-out must be after check-in date.");
+        }
+        else{
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+
+            System.out.println("Check-in data (dd/mm/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.println("Check-out data (dd/mm/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+
+            String error = reservation.updateDates(checkIn, checkOut);
+            if(error != null){
+                System.out.println("Error in reservation: " +error);
+            }
+            else{
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+
+
+        sc.close();
     }
 }

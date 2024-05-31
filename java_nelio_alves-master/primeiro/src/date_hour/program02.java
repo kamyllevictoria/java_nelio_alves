93b485b70b9bc22efd8de7936b8cbd7c56664ab1
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-05-30 22:59:13 -0300

    date-hour studies part II

--- java_nelio_alves-master/primeiro/src/date_hour/program02.java
+++ java_nelio_alves-master/primeiro/src/date_hour/program02.java
@@ -1,7 +1,27 @@
 package date_hour;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class program02 {
     public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+        DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+
+        System.out.println(d04.format(fmt1));
+        System.out.println(d05.format(fmt2));
+        System.out.println(fmt3.format(d06));
+        System.out.println(d05.format(fmt4));
+
+        //System.out.println(fmt2.format(d06));
     }
 }

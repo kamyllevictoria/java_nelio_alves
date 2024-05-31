224fde7767738d218283940f2466c4cd4940c97d
Kamylle Victoria Santos <sharmankamylle@gmail.com>
2024-05-31 13:48:28 -0300

    date-hour studies part III

--- java_nelio_alves-master/primeiro/src/date_hour/program04.java
+++ java_nelio_alves-master/primeiro/src/date_hour/program04.java
@@ -1,18 +1,44 @@
 package date_hour;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class program04 {
     public static void main(String[] args) {
 
         LocalDate d04 = LocalDate.parse("2022-07-20");
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+
         Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        //Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
 
         LocalDate pastWeek = d04.minusWeeks(1);
+        LocalDate nextWeek = d04.plusWeeks(1);
+
+        LocalDateTime pastWeekLocalDateTime = d05.minusWeeks(1);
+        LocalDateTime nextWeekLocalDateTime = d05.plusWeeks(1);
+
+        Duration t1 = Duration.between(pastWeek.atStartOfDay(), d04.atStartOfDay());
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        Duration t4 = Duration.between(d06, pastWeekInstant);
+
         System.out.println(pastWeek);
+        System.out.println(nextWeek);
+        System.out.println(pastWeekLocalDateTime);
+        System.out.println(nextWeekLocalDateTime);
+        System.out.println(t1.toDays());
+        //nao tem como calcular temo de duração com o localdate, so com o localdayetime
+        //realizamos a conversão com o alTime no durantion t1 ou podemos usar o atStratOfTheDay
+        //é menos gambiarra
+        System.out.println(t2.toDays());
+        System.out.println(t3.toDays());
+        System.out.println(t4.toDays());
+
 
     }
 }
--- java_nelio_alves-master/primeiro/src/error_treatment/model_entities/Reservation.java
+++ java_nelio_alves-master/primeiro/src/error_treatment/model_entities/Reservation.java
@@ -10,50 +10,14 @@ public class Reservation {
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 9cfe63e718868f60511485f95a7ffbeba1c2b84f
     private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
         if (!checkOut.after(checkIn)) {
             throw new DomainException("Check-out must be after check-in date.");
         }
-<<<<<<< HEAD
-=======
-=======
-    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
->>>>>>> 559f346fd1cc2b317827fa176462f3fb5257e6f3
->>>>>>> 9cfe63e718868f60511485f95a7ffbeba1c2b84f
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
-        this.checkOut = checkOut;
-    }
-
-    public Integer getRoomNumber() {
-        return roomNumber;
-    }
-
-    public void setRoomNumber(Integer roomNumber) {
-        this.roomNumber = roomNumber;
-    }
-
-    public Date getCheckIn() {
-        return checkIn;
-    }
-
-    public void setCheckIn(Date checkIn) {
-        this.checkIn = checkIn;
-    }
-
-    public Date getCheckOut() {
-        return checkOut;
-    }
-
-    public void setCheckOut(Date checkOut) {
         this.checkOut = checkOut;
     }
 
@@ -72,25 +36,10 @@ public class Reservation {
                 + sdf.format(checkOut)
                 + "\n"
                 + duration()
-<<<<<<< HEAD
                 + " nights";
     }
 
     public void updateDates(Date checkIn, Date checkOut) throws DomainException {
-=======
-                + " nigths: ";
-<<<<<<< HEAD
-=======
-    }
-
-    public void updateDates(Date checkIn, Date checkOut) {
-        this.checkOut = checkOut;
-        this.checkIn = checkIn;
->>>>>>> 559f346fd1cc2b317827fa176462f3fb5257e6f3
-    }
-
-    public void updateDates(Date checkIn, Date checkOut) throws DomainException{
->>>>>>> 9cfe63e718868f60511485f95a7ffbeba1c2b84f
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)) {
             throw new DomainException("Reservation dates for update must be future dates.");

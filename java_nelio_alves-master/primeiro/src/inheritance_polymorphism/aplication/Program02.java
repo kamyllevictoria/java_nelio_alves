f699c1d0939862daeed536b1ece8a3a64f90a2b8
Kamylle Victoria <sharmankamylle@gmail.com>
2024-07-02 13:00:04 -0300

    polymorphism exercise 1

--- java_nelio_alves-master/primeiro/src/inheritance_polymorphism/aplication/Program02.java
+++ java_nelio_alves-master/primeiro/src/inheritance_polymorphism/aplication/Program02.java
@@ -1,22 +1,19 @@
 package inheritance_polymorphism.aplication;
 
 import inheritance_polymorphism.entities.Account;
-import inheritance_polymorphism.entities.BusinessAccount;
+//import inheritance_polymorphism.entities.BusinessAccount;
 import inheritance_polymorphism.entities.SavingAccount;
 
 public class Program02 {
     public static void main(String[] args) {
 
-        Account acc1 = new Account(1010, "Alex", 1000.0);
-        acc1.withdraw(200.0);
-        System.out.println(acc1.getBalance());
+        Account x = new Account(1020, "Ana", 1000.0);
+        Account y = new SavingAccount(1023, "Mary", 1000.0, 0.01);
 
-        Account acc2 = new SavingAccount(1011,"Pedro", 1000.0, 0.01);
-        acc2.withdraw(200);
-        System.out.println(acc2.getBalance());
+        x.withdraw(50.0);
+        y.withdraw(50.0);
 
-        Account acc3 = new BusinessAccount(1012, "Lucas", 1000.0,500.0);
-        acc3.withdraw(200.0);
-        System.out.println(acc3.getBalance());
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
---
+++ java_nelio_alves-master/primeiro/src/inheritance_polymorphism/entities/OutsourcedEmployee.java
@@ -0,0 +1,18 @@
+package inheritance_polymorphism.entities;
+
+public class OutsourcedEmployee extends Employee{
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+    public OutsourcedEmployee(){
+    }
+
+    @Override
+    public Double payment(){
+        return super.payment() + additionalCharge * 1.1;
+    }
+
+}
